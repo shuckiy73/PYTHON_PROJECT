640f54aaf315f7d5ebb1c66c95b5b7997a0edf7f
Dmitry Shchutsky <121363089+shuckiy73@users.noreply.github.com>
2024-02-02 20:25:29 +0300

    init python junior

---
+++ lesson25.py
@@ -0,0 +1,17 @@
+# set_example = {12, 2, 3, 4}
+tuple_example = (1, 2, 3, 4)
+
+# print(set_example)
+# print(set_example.pop())
+# print(set_example)
+# print(set_example.pop())
+# print(set_example)
+# print(set_example.pop())
+# print(set_example)
+
+
+list_example = [1, 2, 3, 4]
+list_example.append(100)
+list_example.append(200)
+
+print(list_example)
