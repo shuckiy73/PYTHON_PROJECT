0a3c6349ccbc1cec650b1737c7a98ce895521eaf
shuckiy73 <121363089+shuckiy73@users.noreply.github.com>
2024-02-11 18:52:39 +0300

    'practica_2'

---
+++ Python OOP/lesson 26/runner.py
@@ -0,0 +1,20 @@
+# from utils
+import Utils
+from repository import StudentsRepoXlsx
+from test_service import TestService
+
+
+class TestProgramRunner:
+    repo = StudentsRepoXlsx()
+    test_service = TestService(repo)
+
+    def run(self):
+        students = self.repo.load_students()
+        login, password = Utils.input_login_pass()
+        student = Utils.check_is_registered(students, login, password)
+        config = self.repo.get_config()
+        self.test_service.try_to_show_menu(students, student, config['menu'])
+
+
+runner = TestProgramRunner()
+runner.run()
